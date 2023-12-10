52136766e6f6f04ea45433b0893bccf8f958de3d
Umer4444 <mumermalik1245@gmail.com>
2023-12-11 00:33:21 +0500

    home page

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.home),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('category/<str:category>/', views.category_products, name='category_products'),
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -11,3 +11,7 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     product_images = ProductImage.objects.filter(product=product)
     return render(request, 'myapp/product_detail.html', {'product': product, 'product_images': product_images})
+
+def category_products(request, category):
+    products = Product.objects.filter(category=category)
+    return render(request, 'myapp/category_products.html', {'products': products})
