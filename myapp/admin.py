fed962f05df771d6b66a1a0478d018e27330f237
Umer4444 <mumermalik1245@gmail.com>
2023-12-12 22:48:16 +0500

    Done the cart

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, ProductImage
+from .models import *
 
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
@@ -8,3 +8,9 @@ class ProductImageInline(admin.TabularInline):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageInline]
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    pass  # You can add more configurations or fields to display here
+
+
